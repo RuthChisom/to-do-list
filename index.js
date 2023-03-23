e7bcc9a9e10be47dcab8a070a48a3d26633d190e
Ruth Chisom <ndukwe100805043@gmail.com>
2023-03-23 11:45:48 +0100

    Remove item from list

--- index.js
+++ index.js
@@ -17,9 +17,22 @@ const addItem = () => {
         alert("Please enter an item!!"); //prevent empty values from being added to the list
     }
     else{
-        let li = document.createElement("li");
-        var text = document.createTextNode(inputValue.value);
-        li.append(text);
+        let li = document.createElement("li");  //create li tag
+        let text = document.createTextNode(inputValue.value);   //input text
+
+        let remove = document.createElement("button");  //create remove button
+        remove.innerHTML = "Remove";    //label remove button
+        // function to remove
+        remove.onclick = function(){
+            // alert('here be dragons');return false;
+            li.parentNode.removeChild(li);
+          };
+
+        let edit = document.createElement("button");  //create edit button
+        edit.innerHTML = "Edit";    //label edit button
+        li.append(text);    //add text to list
+        li.append(remove);  //add remove button to list
+        li.append(edit);  //add edit button to list
         toDoList.insertBefore(li,toDoList.firstElementChild); //shows the most recently added item first on the list
         // document.getElementById('myUL').appendChild(li);  //shows the most recently added item last on the list
 
